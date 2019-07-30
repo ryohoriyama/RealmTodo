6e3d947328fb679eff264bb35e3df60dd3efa9a5
Ryo Horiyama <>
2019-07-30 11:35:54 +0800

    ファイル作成

---
+++ RalmTodo/Controllers/InputViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InputViewController.swift
+//  RalmTodo
+//
+//  Created by Ryo Horiyama on 2019/07/30.
+//  Copyright © 2019 Ryo Horiyama. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RalmTodo/Objects/Todo.swift
@@ -0,0 +1,9 @@
+//
+//  Todo.swift
+//  RalmTodo
+//
+//  Created by Ryo Horiyama on 2019/07/30.
+//  Copyright © 2019 Ryo Horiyama. All rights reserved.
+//
+
+import Foundation
